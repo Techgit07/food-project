7eec23a77158c250434fa5b14a025f34e3f0bd75
Techgit07 <kaushalgithub@gmail.com>
2022-10-18 23:26:24 +0530

    lookup changes

--- src/controllers/user/user.ts
+++ src/controllers/user/user.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { Request, response, Response } from "express";
 import { foodcategoryModel, foodproductModel, userModel, } from "../../database";
 import { apiResponse } from "../../common";
 import { responseMessage } from "../../helper";
@@ -9,17 +9,75 @@ import jwt from 'jsonwebtoken';
 const ObjectId = require('mongoose').Types.ObjectId;
 const jwt_token_secret = config.get('jwt_token_secret');
 
+// export const getProduct = async (req: Request, res: Response) => {
+//     try {
+//         let response = await foodproductModel.find({ isActive: true })
+//         if (response) {
+//             return res.status(200).json(new apiResponse(200, responseMessage?.getDataSuccess("product"), response, {}))
+//         } else {
+//             return res.status(404).json(new apiResponse(404, responseMessage?.getDataNotFound("product"), null, {}))
+//         }
+//     } catch (error) {
+//         console.log("error", error);
+//         return res.status(500).json(new apiResponse(500, responseMessage?.internalServerError, null, {}))
+//     }
+// }
+
 export const getProduct = async (req: Request, res: Response) => {
+    let response: any = {}
     try {
-        let response = await foodproductModel.find({ isActive: true })
+        let user: any = req.headers.user
+        response = await foodproductModel.aggregate([
+            { $match: { isActive: true } },
+            {
+                $lookup: {
+                    from: "categories",
+                    let: { id: "$categoryId" },
+                    pipeline: [
+                        {
+                            $match: {
+                                $expr: {
+                                    $and: [
+                                        { $eq: ['$_id', '$$id'] },
+                                        { $eq: ['$isActive', true] },
+                                    ]
+                                }
+                            }
+                        },
+                    ],
+                    as: "categoryData"
+                },
+            },
+            // {
+            //     $lookup: {
+            //         from: "users",
+            //         let: { id: "$createdBy" },
+            //         pipeline: [
+            //             {
+            //                 $match: {
+            //                     $expr: {
+            //                         $and: [
+            //                             { $eq: ['$_id', '$$id'] },
+            //                             { $eq: ['$isActive', true] },
+            //                         ]
+            //                     }
+            //                 }
+            //             },
+            //         ],
+            //         as: "createdBy"
+            //     },
+            // },
+            // {
+            //     $project: {
+            //         userData: 1, productData: 1, _id: 1, quantity: 1, foodSize: 1, total: 1, orderedBy: 1, createdAt: 1,
+            //     }
+            // }
+        ])
         if (response) {
-            return res.status(200).json(new apiResponse(200, responseMessage?.getDataSuccess("product"), response, {}))
-        } else {
-            return res.status(404).json(new apiResponse(404, responseMessage?.getDataNotFound("product"), null, {}))
+            return res.status(200).send(new apiResponse(200, responseMessage?.getDataSuccess('food'), { response }, {}))
         }
     } catch (error) {
-        console.log("error", error);
-        return res.status(500).json(new apiResponse(500, responseMessage?.internalServerError, null, {}))
+        return res.status(500).send(new apiResponse(500, responseMessage?.internalServerError, {}, {}))
     }
 }
 
