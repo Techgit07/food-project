9a70d187c682bc2207907ec9890b4d022451119d
Techgit07 <kaushalnena2000@gmail.com>
2022-10-19 11:30:03 +0530

    add to cart

--- src/controllers/user/user.ts
+++ src/controllers/user/user.ts
@@ -9,17 +9,55 @@ import jwt from 'jsonwebtoken';
 const ObjectId = require('mongoose').Types.ObjectId;
 const jwt_token_secret = config.get('jwt_token_secret');
 
+// export const getProduct = async (req: Request, res: Response) => {
+//     try {
+//         let response = await foodproductModel.find({ isActive: true })
+//         if (response) {
+//             return res.status(200).json(new apiResponse(200, responseMessage?.getDataSuccess("product"), response, {}))
+//         } else {
+//             return res.status(404).json(new apiResponse(404, responseMessage?.getDataNotFound("product"), null, {}))
+//         }
+//     } catch (error) {
+//         console.log("error", error);
+//         return res.status(500).json(new apiResponse(500, responseMessage?.internalServerError, null, {}))
+//     }
+// }
+
 export const getProduct = async (req: Request, res: Response) => {
     try {
-        let response = await foodproductModel.find({ isActive: true })
+        let user: any = req.headers.user,
+            response = await foodproductModel.aggregate([
+                { $match: { isActive: true } },
+                {
+                    $lookup: {
+                        from: "categories",
+                        let: { id: "$categoryId" },
+                        pipeline: [
+                            {
+                                $match: {
+                                    $expr: {
+                                        $and: [
+                                            { $eq: ['$_id', '$$id'] },
+                                            { $eq: ['$isActive', true] },
+                                        ]
+                                    }
+                                }
+                            },
+                        ],
+                        as: "categoryData"
+                    },
+                },
+                {
+                    $project: {
+                        categoryData: 1, description: 1, price: 1, createdBy: 1, createdAt: 1
+                    }
+                }
+            ])
         if (response) {
-            return res.status(200).json(new apiResponse(200, responseMessage?.getDataSuccess("product"), response, {}))
-        } else {
-            return res.status(404).json(new apiResponse(404, responseMessage?.getDataNotFound("product"), null, {}))
+            return res.status(200).send(new apiResponse(200, responseMessage?.getDataSuccess('product'), { response }, {}))
         }
     } catch (error) {
-        console.log("error", error);
-        return res.status(500).json(new apiResponse(500, responseMessage?.internalServerError, null, {}))
+        return res.status(500).send(new apiResponse(500, responseMessage?.internalServerError, {}, {}))
     }
 }
 
